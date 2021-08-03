9d384d57c119dfc4de253d8b1481562d7cb16b7e
randy140801 <randy140801@gmail.com>
2021-08-02 20:47:05 -0400

    Menu functionality

---
+++ Assets/Prefabs/Menu/Menushoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class Menushoot : MonoBehaviour
+{
+    void Update ()
+    {
+
+    }
+
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void close()
+    {
+        Application.Quit();
+    }
+
+}
